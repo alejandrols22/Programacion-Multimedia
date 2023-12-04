f87f2f1c1c130b6ca8b628314d87f52e49b3503e
alejandrols22 <alejandrolopsep@gmail.com>
2023-12-04 12:15:50 +0100

    Clase 12 de diciembre

---
+++ Tema 5 Kotlin/clase_04_12_23_android/app/src/test/java/EjercicioHerenciaToStringHashCode/Ejercicio2HerenciaToStringHashCode.kt
@@ -0,0 +1,54 @@
+package EjercicioHerenciaToStringHashCode
+
+
+open class ProductoInformatico(val nombre: String, val precio: Double) {
+    fun encender() {
+        println("$nombre encendido.")
+    }
+
+    fun apagar() {
+        println("$nombre apagado.")
+    }
+
+    open fun ejecutar() {
+        println("$nombre ejecutando operaciones generales.")
+    }
+}
+
+
+class Laptop(nombre: String, precio: Double, val marca: String) : ProductoInformatico(nombre, precio) {
+    override fun ejecutar() {
+        super.ejecutar()
+        println("Marca de la laptop: $marca")
+    }
+}
+
+
+class Impresora(nombre: String, precio: Double, val tipo: String) : ProductoInformatico(nombre, precio) {
+    fun imprimir() {
+        println("$nombre imprimiendo documento.")
+    }
+
+    override fun ejecutar() {
+        super.ejecutar()
+        println("Tipo de impresora: $tipo")
+    }
+}
+
+
+fun main() {
+    val laptop = Laptop("HP Envy", 999.99, "HP")
+    laptop.encender()
+    laptop.apagar()
+    laptop.ejecutar()
+
+    println()
+
+    val impresora = Impresora("Epson EcoTank", 299.99, "Inyección de tinta")
+    impresora.encender()
+    impresora.apagar()
+    impresora.ejecutar()
+    impresora.imprimir()
+
+
+}
---
+++ Tema 5 Kotlin/clase_04_12_23_android/settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "clase_04_12_23_android"
+include(":app")
+include(":app")
+ 
