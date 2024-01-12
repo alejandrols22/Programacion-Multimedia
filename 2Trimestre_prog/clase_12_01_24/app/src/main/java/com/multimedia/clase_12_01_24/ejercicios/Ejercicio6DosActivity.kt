76cc7bfe43d4f22f57c13d85867f53965ac0088f
alejandrols22 <alejandrolopsep@gmail.com>
2024-01-12 11:32:30 +0100

    correcion codigo
    correcion codigo

---
+++ 2Trimestre_prog/clase_12_01_24/app/src/main/java/com/multimedia/clase_12_01_24/ejercicios/Ejercicio6DosActivity.kt
@@ -0,0 +1,12 @@
+package com.multimedia.clase_12_01_24.ejercicios
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.multimedia.clase_12_01_24.R
+
+class Ejercicio6DosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejercicio6_dos)
+    }
+}
---
+++ 2Trimestre_prog/clase_12_01_24/app/src/main/java/com/multimedia/clase_12_01_24/ejercicios/ResultadoEjercicio6DosActivity.kt
@@ -0,0 +1,12 @@
+package com.multimedia.clase_12_01_24.ejercicios
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.multimedia.clase_12_01_24.R
+
+class ResultadoEjercicio6DosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado_ejercicio6_dos)
+    }
+}
